ca26a74c0c8804e42f9ea9f4691dd74dbadb7f66
Gustavo Funchal <delagusttaf@gmail.com>
2021-03-01 23:29:44 -0300

    Adding comments on posts

--- src/main/java/com/gustavofunchal/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gustavofunchal/workshopmongo/config/Instantiation.java
@@ -1,54 +1,74 @@
 package com.gustavofunchal.workshopmongo.config;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.TimeZone;
-
+import com.gustavofunchal.workshopmongo.domain.Post;
+import com.gustavofunchal.workshopmongo.domain.User;
 import com.gustavofunchal.workshopmongo.dto.AuthorDTO;
+import com.gustavofunchal.workshopmongo.dto.CommentDTO;
+import com.gustavofunchal.workshopmongo.repositories.PostRepository;
+import com.gustavofunchal.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
-import com.gustavofunchal.workshopmongo.domain.Post;
-import com.gustavofunchal.workshopmongo.domain.User;
-import com.gustavofunchal.workshopmongo.repositories.PostRepository;
-import com.gustavofunchal.workshopmongo.repositories.UserRepository;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
-	private UserRepository userRepository;
-	private PostRepository postRepository;
+  private UserRepository userRepository;
+  private PostRepository postRepository;
 
-	@Autowired
-	public Instantiation(UserRepository userRepository, PostRepository postRepository) {
-		this.userRepository = userRepository;
-		this.postRepository = postRepository;
-	}
+  @Autowired
+  public Instantiation(UserRepository userRepository, PostRepository postRepository) {
+    this.userRepository = userRepository;
+    this.postRepository = postRepository;
+  }
 
-	@Override
-	public void run(String... args) throws Exception {
+  @Override
+  public void run(String... args) throws Exception {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		userRepository.deleteAll();
-		postRepository.deleteAll();
+    userRepository.deleteAll();
+    postRepository.deleteAll();
 
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+    User maria = new User(null, "Maria Brown", "maria@gmail.com");
+    User alex = new User(null, "Alex Green", "alex@gmail.com");
+    User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP, abraços.", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+    Post post1 =
+        new Post(
+            null,
+            sdf.parse("21/03/2018"),
+            "Partiu viagem",
+            "Vou viajar para SP, abraços.",
+            new AuthorDTO(maria));
 
-		postRepository.saveAll(Arrays.asList(post1, post2));
+    Post post2 =
+        new Post(
+            null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		userRepository.save(maria);
+    CommentDTO c1 =
+        new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2021"), new AuthorDTO(alex));
 
-	}
+    CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2021"), new AuthorDTO(bob));
 
+    CommentDTO c3 =
+        new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2021"), new AuthorDTO(alex));
+
+    post1.getComments().addAll(Arrays.asList(c1, c2));
+    post2.getComments().addAll(singletonList(c3));
+
+    postRepository.saveAll(Arrays.asList(post1, post2));
+
+    maria.getPosts().addAll(Arrays.asList(post1, post2));
+    userRepository.save(maria);
+  }
 }
--- src/main/java/com/gustavofunchal/workshopmongo/domain/Post.java
+++ src/main/java/com/gustavofunchal/workshopmongo/domain/Post.java
@@ -1,17 +1,21 @@
 package com.gustavofunchal.workshopmongo.domain;
 
 import com.gustavofunchal.workshopmongo.dto.AuthorDTO;
+import com.gustavofunchal.workshopmongo.dto.CommentDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @Document
-@AllArgsConstructor
+@NoArgsConstructor
 public class Post implements Serializable {
   private static final long serialVersionUID = 1L;
 
@@ -20,4 +24,13 @@ public class Post implements Serializable {
   private String title;
   private String body;
   private AuthorDTO author;
+  private List<CommentDTO> comments = new ArrayList<>();
+
+  public Post(String id, Date date, String title, String body, AuthorDTO author) {
+    this.id = id;
+    this.date = date;
+    this.title = title;
+    this.body = body;
+    this.author = author;
+  }
 }
